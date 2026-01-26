48bb6d5716541de86e19640cb861a2af90ac21ad
Felix St-Denis <felix.st-denis@ec.gc.ca>
2026-01-26 21:13:10 +0000

    Date time utilities
    The analysis can now support the reading and analysis of multiple time
    stamps. It can read/analyse CICE outputs from a startdate to and endate
    at a certain frequency freq.
    
    - TimeUtilities.py was modified.

--- ciceplotting/ciceplotting/utils/TimeUtilities.py
+++ ciceplotting/ciceplotting/utils/TimeUtilities.py
@@ -1,10 +1,53 @@
 import numpy as np
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
 
 class TimeUtility:
 
-    def __init__(self, configuration_time = None, 
-                    configuration_fig = None):
+    def __init__(self, configuration_time = None):
         
         self.dt = int(configuration_time['dt'])
-        self.outputdir = int(configuration_fig['outputdir'])
+        self.startdate = str(configuration_time['startdate'])
+        self.enddate = str(configuration_time['enddate'])
+        self.freq = str(configuration_time['freq'])
+
+    def setup_time(self):
+        
+        start = datetime.strptime(self.startdate, self.freq)
+        end = datetime.strptime(self.enddate, self.freq)
+
+        dates = self.dates_list(start,end,self.freq)
+
+        return dates
+    
+    def infer_step(self, fmt):
+
+        if '%d' in fmt:
+            return timedelta(days = 1)
+        
+        elif '%m' in fmt:
+            return relativedelta(months=1)
+        
+        elif '%Y' in fmt:
+            return relativedelta(years=1)
+        
+        else:
+            return 'unknown'
+
+    def dates_list(self, start_date, end_date, freq):
+
+        step = self.infer_step(freq)
+        
+        dates = []
+        current = start_date
+        while current <= end_date:
+            dates.append(current.strftime(freq))
+            current += step
+
+        return dates
+    
+
+
+
 
