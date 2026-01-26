69513b0af68d992d97d714b6f14ff7f6b48adf77
Felix St-Denis <felix.st-denis@ec.gc.ca>
2026-01-26 21:08:12 +0000

    Comments for reading data
    Added comments to the readdate function.
    
    - readdata.py was modified.

--- ciceplotting/ciceplotting/data/readdata.py
+++ ciceplotting/ciceplotting/data/readdata.py
@@ -2,6 +2,14 @@ import numpy as np
 import xarray as xr
 
 def readdata(exp, datestr, var, datadir):
+    '''
+    Function used to open the .nc file from CICE
+
+    :param exp: experiment that will be read 
+    :param datestr: date open
+    :param var: variable of the output file
+    :param datadir: directory of the output file
+    '''
 
     file = exp + '_' + var + '.' + datestr + '.nc'
 
@@ -9,4 +17,10 @@ def readdata(exp, datestr, var, datadir):
     #print(xr.open_dataset('/home/fsd001/analysis/ciceplotting/B15mink1_480_iceh_inst.2005-01-02-00000.nc', decode_times = False))
 
     return dataset_exp
+
+
+
+
+
+
 
