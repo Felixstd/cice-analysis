3360cde6b9305f02cdce0c4dae1f6590f3e62b3e
Felix St-Denis <felix.st-denis@ec.gc.ca>
2026-01-26 21:17:20 +0000

    Loops and plotting calls
    Modified how the analysis is done. The code reads by experiments and
    then loops through the dates. It saves the data in one dictionnary.
    Depending on the case analysed, it can either plot 1d or 2d fields (IMEX
    or Global).
    
    - cice_analysis.py was modified.

--- ciceplotting/cice_analysis.py
+++ ciceplotting/cice_analysis.py
@@ -1,3 +1,19 @@
+
+"""
+CICE-Analysis
+
+This package is used to analyse the outputs of the CICE sea ice model. 
+The different functions are located in ciceplotting. It contains
+three repositories:
+
+- data: everything that as to do with reading data and namelist. 
+- plotting: functions to plot and create figures.
+- utils: utilities such as time management. 
+
+Felix St-Denis (09/01/2026)
+
+"""
+
 import numpy as np
 import xarray as xr 
 import matplotlib.pyplot as plt
@@ -8,54 +24,135 @@ import matplotlib.cm as pltcm
 from ciceplotting.utils.TimeUtilities import TimeUtility
 from ciceplotting.data.readnamelist import namelist
 from ciceplotting.data import readdata
-
+from ciceplotting.plotting import plot_cice
+from collections import defaultdict
 
 
 import configparser
-import cmocean as cm
+import cmocean as cmo
 import warnings
 import os
 
 
-plt.style.use('/home/fsd001/analysis/ciceplotting/ciceplotting/utils/science.mplstyle')
+#configuration of  the style of figures
+plt.style.use('/home/fsd001/cice-analysis/ciceplotting/ciceplotting/plotting/science.mplstyle')
 
 
-config_exp = configparser.ConfigParser()
+config_exp = configparser.ConfigParser(interpolation=None)
 config_exp.read('./namelist')
 
-
+#reading namelist
 Parameters = namelist(configuration_exp  = config_exp['Experiment'], 
                       configuration_dyn  = config_exp['Dynamics'], 
                       configuration_num  = config_exp['Numerical'],
                       configuration_fig  = config_exp['Figures'], 
                       configuration_time = config_exp['Time'])
 
+Dates_Config = TimeUtility(configuration_time = config_exp['Time'])
+dates_list = TimeUtility.setup_time(Dates_Config)
 
-uvels_experiment = []
+print(dates_list)
+uvels_experiments = []
+h_experiments = []
 
+data_experiments = defaultdict(dict)
+
+if not os.path.isdir(Parameters.figdir+Parameters.case):
+    os.mkdir(Parameters.figdir+Parameters.case)
+
+#going through all experiments
 for exp in Parameters.exp:
 
-    print('Reading Experiment: ', exp)
+    print('Experiment: ', exp)
 
-    # if not os.path.isdir(Parameters.figdir+str(exp)):
-    #     os.mkdir(Parameters.figdir+str(exp))
+    if not os.path.isdir(Parameters.figdir+'Experiments/'+str(exp)):
+        os.mkdir(Parameters.figdir+'Experiments/'+str(exp))
 
+    #reading data 
     if Parameters.read_all:
-        dataset_exp = readdata.readdata(exp, 
-                                     Parameters.datestr, 
-                                     Parameters.var, 
-                                     Parameters.datadir)
-        
-        uvel = dataset_exp['uvel']
-        uvels_experiment.append(uvel)
+        print('Reading Data')
+
+        for datestr in dates_list:
+            print('Reading Date: ', datestr)
+            dataset_exp = readdata.readdata(exp, 
+                                        datestr, 
+                                        Parameters.var, 
+                                        Parameters.datadir)
+
+            data_experiments[exp][datestr] = dataset_exp
+
+
+print('Plotting Figures')
+
+if Parameters.Global:
+
+    datestr = '2005-01-02'
+
+    plot_cice.plot_global(data_experiments, 
+                        'Cgrid', 
+                        datestr, 
+                        'vvel', 
+                        Parameters,
+                        r'$v_{ice}$ (m/s)', 
+                        'C-grid',
+                        'vvel_cgrid'+'_'+datestr)
+    
+    plot_cice.plot_global(data_experiments, 
+                        'Bgrid', 
+                        datestr, 
+                        'vvel', 
+                        Parameters,
+                        r'$v_{ice}$ (m/s)', 
+                        'B-grid',
+                        'vvel_bgrid'+'_'+datestr)
+
+if Parameters.imex:
+
+    plot_cice.plot_1d(data_experiments, 
+                    exp, 
+                    datestr,
+                    Parameters, 
+                    'hi',
+                    r'$x$ (km)',
+                    r'$h$ (m)', 
+                    'Rothrock',
+                    'h_imex.png', 
+                    IMEX = True)
+
+    #making a figure with multiple experiments
+    print('Plotting Figures')
+    x = np.arange(300, 400)*Parameters.dx
+
+    labels = ['Default', 'IMEXSerial', 'IMEXParallel']
+    #thickness
+    print('Plotting Thicknesses')
+    plt.figure()
+    for i, h in enumerate(h_experiments):
+        plt.plot(x, h[0, 200, 300:], label = Parameters.exp[i])
+    plt.legend()
+    plt.xlabel(r'$x$ (km)')
+    plt.ylabel(r'$h$ (m)')
+    plt.savefig(Parameters.figdir+'/'+Parameters.case+'/h_IMEX'+'.png')
 
 
 
-x = np.arange(300, 400)*Parameters.dx
-plt.figure()
-for i, uvel in enumerate(uvels_experiment):
-    plt.plot(x, uvel[0, 200, 300:], label = Parameters.exp[i])
-plt.legend()
-plt.xlabel(r'$x$ (km)')
-plt.ylabel(r'$u_{ice}$ (m/s)')
-plt.savefig(Parameters.figdir+'/uvel_bgrid'+'.png')
+
+    print('Plotting Velocities')
+    # velocity
+    plt.figure()
+    for i, uvel in enumerate(uvels_experiments):
+        plt.plot(x, uvel[0, 200, 300:], label = Parameters.exp[i])
+    plt.legend()
+    plt.xlabel(r'$x$ (km)')
+    plt.ylabel(r'$u_{ice}$ (m/s)')
+    plt.savefig(Parameters.figdir+'/'+Parameters.case+'/uvel_IMEX'+'.png')
+
+
+    h_diff = h_experiments[0] - h_experiments[1]
+
+    plt.figure()
+    plt.plot(x, h_diff[0, 200, 300:])
+    plt.legend()
+    plt.xlabel(r'$x$ (km)')
+    plt.ylabel(r'$\Delta h$ (m)')
+    plt.savefig(Parameters.figdir+'/'+Parameters.case+'/diffh_IMEX'+'.png')
