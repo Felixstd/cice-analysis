5d650429fa32c85fb86309d872d77117473081e3
Felix St-Denis <felix.st-denis@ec.gc.ca>
2026-02-23 14:41:57 +0000

    read_cicelog: preconditionner
    Can now read the preconditionner norm from the cice runlog. It also
    reads the iteration and takes as output the max iteration for each
    precond/solver loop.

--- ciceplotting/ciceplotting/data/readdata.py
+++ ciceplotting/ciceplotting/data/readdata.py
@@ -15,21 +15,21 @@ def readdata(exp, datestr, var, datadir):
     file = exp + '_' + var + '.' + datestr + '.nc'
 
     dataset_exp = xr.open_dataset(datadir+'/'+file, decode_times = False)
-    #print(xr.open_dataset('/home/fsd001/analysis/ciceplotting/B15mink1_480_iceh_inst.2005-01-02-00000.nc', decode_times = False))
 
     return dataset_exp
 
-def read_cicelog(filename, monitor_nonlin, monitor_fgmres):
+def read_cicelog(filename, solver, precond, monitor_nonlin, monitor_solver, monitor_precond):
 
-    if monitor_nonlin:
+    print('Reading cicelog')
 
-        nonlin_value = []
-        progress_value = []
-
-    if monitor_fgmres : 
-        fgmres_value = []
+    nonlin_value   = []
+    progress_value = []
+    solver_value   = []
+    precond_value  = []
+    its_precond    = []
 
     with open(filename, "r") as f:
+        previous_iter = None
         for line in f:
 
             if monitor_nonlin:
@@ -54,22 +54,38 @@ def read_cicelog(filename, monitor_nonlin, monitor_fgmres):
                         val = float(m.group(2).replace("D", "E"))
                         progress_value.append(val)
 
-            if monitor_fgmres:
-                if "fgmres_L2norm" in line:
+            if monitor_solver:
+                if solver+"_L2norm" in line:
+
                     m = re.search(
-                        r"iter_fgmres=\s*(\d+)\s+fgmres_L2norm=\s*([0-9.D+-]+)",
-                        line
-                    )
+                         rf"iter_{re.escape(solver)}=\s*(\d+)\s+{re.escape(solver)}_L2norm=\s*([0-9.D+-]+)",
+                            line
+                        )
                     if m:
                         it = int(m.group(1))
                         val = float(m.group(2).replace("D", "E"))
-                        fgmres_value.append(val)
+                        solver_value.append(val)
 
-    if monitor_nonlin and monitor_fgmres:
-        return nonlin_value, progress_value, fgmres_value
+                        # print(it)
+            
+            if monitor_precond:
+                if precond+"_L2norm" in line:
+                    m = re.search(
+                         rf"iter_{re.escape(precond)}=\s*(\d+)\s+{re.escape(precond)}_L2norm=\s*([0-9.+-DEde]+)",
+                            line
+                        )
+                    if m:
+                        it = int(m.group(1))
+                        val = float(m.group(2).replace("D", "E"))
+                        precond_value.append(val)
+                        if it == 0 and previous_iter is not None:
+                            its_precond.append(previous_iter)
 
-    else:
-        return nonlin_value, progress_value
+                        previous_iter = it
+    
+    return nonlin_value, progress_value, solver_value, its_precond, precond_value
+
+
 
 
 
