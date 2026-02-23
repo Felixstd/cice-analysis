9c81bab1180c4d3a7b5db88db064a40e19b1d5f2
Felix St-Denis <felix.st-denis@ec.gc.ca>
2026-02-23 14:38:12 +0000

    arguments.py: new arguments
    Added new arguments for the analysis of the efficiency of different
    solvers. Options are:
    
    - experiment(s) name
    - analysing the numerics
    - plotting the velociy(ies)
    - type of grid
    - look at the spatial residual
    - type of solver
    - type of precond

--- ciceplotting/ciceplotting/utils/arguments.py
+++ ciceplotting/ciceplotting/utils/arguments.py
@@ -10,10 +10,33 @@ def parse_args(num = True):
     
     if num:
         #---- For numerical analysis ----#
-        parser = ArgumentParser()
+        parser = ArgumentParser(fromfile_prefix_chars='@')
+
         parser.add_argument("-e", "--exp", dest="exp",
                             help="Experiment to read data from")
+        
+        parser.add_argument('-n', '--numerics', dest = 'num', 
+                            help = 'Analysing the numerics, true or false.')
+        
+        parser.add_argument('-v', '--velocity', dest = 'vel', 
+                            help = 'Plotting the velocity, set to true or false.')
+        
+        parser.add_argument('-g', '--grid', dest = 'grid', 
+                            help = 'The grid on which the data is on, C or B.')
+        
+        parser.add_argument('-r', '--res', dest = 'res', 
+                            help = 'set to true to analyse the residuals spatially ')
 
+        parser.add_argument('-s', '--solver', dest = 'solver', 
+                            help = 'solver used in exp')
+        
+        parser.add_argument('-p', '--precond', dest = 'precond', 
+                            help = 'Was a precond used in the run, True or False')
+        
+        parser.add_argument('-P', '--precondname', dest = 'precondname', 
+                            help = 'name of the precond used in the run')
+        
+        
         args = parser.parse_args()
 
     return args
