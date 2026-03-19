c52de82f0771a0927f484f42539ec54ff71a63e8
Felix St-Denis <felix.st-denis@ec.gc.ca>
2026-03-19 18:45:47 +0000

    reading cicelog: reading multiple days
    The read_cicelog has been greatly modified especially to read the
    cicelog for multiple days and for blocks of the cicelog.
    
    A new function has been added:
    - find_block_cicelog: finds the lines corresponding to specific dates in
      the cicelog such that we can compare more easily with different
      experiment

--- ciceplotting/ciceplotting/data/readdata.py
+++ ciceplotting/ciceplotting/data/readdata.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xarray as xr
 import re
+from ciceplotting.utils import manip_arrays
 
 def readdata(exp, datestr, var, datadir):
     '''
@@ -90,7 +91,8 @@ def read_cicelog_dynamics(filename):
           vvel_point, strintx, strinty, strocnx, strocny, strairx, strairy
 
 
-def read_cicelog(filename, solver, precond, monitor_nonlin, monitor_solver, monitor_precond):
+def read_cicelog(filename, solver, precond, monitor_nonlin, monitor_solver, 
+                 monitor_precond, startdate = None, enddate = None, block_lines = False ):
 
     print('Reading cicelog')
 
@@ -101,76 +103,141 @@ def read_cicelog(filename, solver, precond, monitor_nonlin, monitor_solver, moni
     its_precond    = []
     its_solver     = []
     breakdowns_it  = []
+    solver_its_per_cycle = []
+    fgmres_per_nonlin = []
+
+    nonlin_cycles  = []
+    current_nl_cycle = []
+    solver_cycles  = []
+    _current_cycles= []
+    count_fgmres = 0
 
     with open(filename, "r") as f:
+
+        lines = f.readlines()
+
+    if block_lines:
+        lines = find_block_cicelog(lines,startdate,enddate)
+        previous_iter = 0
+        previous_iter_nonlin = 0
+        previous_iter_solver = 0
+    else:
         previous_iter = None
-        for line in f:
+        previous_iter_nonlin = None
+        previous_iter_solver = None
+   
+    for line in lines:
 
-            if monitor_nonlin:
-                if "nonlin_res_L2norm" in line:
-                    m = re.search(
-                        r"iter_nonlin=\s*(\d+)\s+nonlin_res_L2norm=\s*([0-9.D+-]+)",
+        if monitor_nonlin:
+            if 'monitor_fgmres: iter_fgmres=   0' in line:
+                count_fgmres+=1
+            if "nonlin_res_L2norm" in line:
+                m = re.search(
+                    r"iter_nonlin=\s*(\d+)\s+nonlin_res_L2norm=\s*([0-9.D+-]+)",
+                    line
+                )
+                if m:
+                    it_nonlin = int(m.group(1))
+                    val = float(m.group(2).replace("D", "E"))
+                    nonlin_value.append(val)
+
+
+                    if (it_nonlin == 0 and previous_iter_nonlin is not None):
+                        # if block_lines:
+                            
+                        nonlin_cycles.append(current_nl_cycle)
+                        fgmres_per_nonlin.append(count_fgmres)
+                        current_nl_cycle = []
+                        count_fgmres = 0
+
+                    current_nl_cycle.append(val)
+                    previous_iter_nonlin = it_nonlin
+
+            # progress residual
+            elif "progress_res_L2norm" in line:
+                m = re.search(
+                    r"iter_nonlin=\s*(\d+)\s+progress_res_L2norm=\s*([0-9.D+-]+)",
+                    line
+                )
+                if m:
+                    it = int(m.group(1))
+                    val = float(m.group(2).replace("D", "E"))
+                    progress_value.append(val)
+
+        if monitor_solver:
+            if solver+"_L2norm" in line:
+
+                m = re.search(
+                        rf"iter_{re.escape(solver)}=\s*(\d+)\s+{re.escape(solver)}_L2norm=\s*([0-9.D+-]+)",
                         line
                     )
-                    if m:
-                        it = int(m.group(1))
-                        val = float(m.group(2).replace("D", "E"))
-                        nonlin_value.append(val)
+                if m:
+                    it_solver = int(m.group(1))
+                    val = float(m.group(2).replace("D", "E"))
+                    solver_value.append(val)
+                    # print(it, previous_iter)
+                    if it_solver == 0 and previous_iter_solver is not None:
+                        its_solver.append(previous_iter)
+                        solver_cycles.append(_current_cycles)
+                        solver_its_per_cycle.append(previous_iter_solver)
+                        _current_cycles = []
 
-                # progress residual
-                elif "progress_res_L2norm" in line:
-                    m = re.search(
-                        r"iter_nonlin=\s*(\d+)\s+progress_res_L2norm=\s*([0-9.D+-]+)",
+                    _current_cycles.append(val)
+
+                    previous_iter_solver = it_solver
+
+
+            if solver == 'BiCGSTABEll':
+                m = re.search(
+                            r"number_of_breakdowns\s*=\s*(\d+)", line
+                )
+                if m :
+                    it = int(m.group(1))
+                    breakdowns_it.append(it)
+
+                    
+        
+        if monitor_precond:
+            if precond+"_L2norm" in line:
+                m = re.search(
+                        rf"iter_{re.escape(precond)}=\s*(\d+)\s+{re.escape(precond)}_L2norm=\s*([0-9.+-DEde]+)",
                         line
                     )
-                    if m:
-                        it = int(m.group(1))
-                        val = float(m.group(2).replace("D", "E"))
-                        progress_value.append(val)
+                if m:
+                    it = int(m.group(1))
+                    val = float(m.group(2).replace("D", "E"))
+                    precond_value.append(val)
+                    if it == 0 and previous_iter is not None:
+                        its_precond.append(previous_iter)
 
-            if monitor_solver:
-                if solver+"_L2norm" in line:
+                    previous_iter = it
 
-                    m = re.search(
-                         rf"iter_{re.escape(solver)}=\s*(\d+)\s+{re.escape(solver)}_L2norm=\s*([0-9.D+-]+)",
-                            line
-                        )
-                    if m:
-                        it = int(m.group(1))
-                        val = float(m.group(2).replace("D", "E"))
-                        solver_value.append(val)
-                        if it == 1 and previous_iter is not None:
-                            its_solver.append(previous_iter)
-
-                        previous_iter = it
-
-
-                if solver == 'BiCGSTABEll':
-                    m = re.search(
-                                r"number_of_breakdowns\s*=\s*(\d+)", line
-                    )
-                    if m :
-                        it = int(m.group(1))
-                        breakdowns_it.append(it)
-
-                        
-            
-            if monitor_precond:
-                if precond+"_L2norm" in line:
-                    m = re.search(
-                         rf"iter_{re.escape(precond)}=\s*(\d+)\s+{re.escape(precond)}_L2norm=\s*([0-9.+-DEde]+)",
-                            line
-                        )
-                    if m:
-                        it = int(m.group(1))
-                        val = float(m.group(2).replace("D", "E"))
-                        precond_value.append(val)
-                        if it == 0 and previous_iter is not None:
-                            its_precond.append(previous_iter)
-
-                        previous_iter = it
+    pad_solver_cycles = manip_arrays.pad_array(solver_cycles)
+    pad_nonlincycles = manip_arrays.pad_array(nonlin_cycles)
     
-    return nonlin_value, progress_value, solver_value, its_solver, its_precond, precond_value, breakdowns_it
+    return nonlin_value, progress_value, solver_value, its_solver, its_precond, precond_value, \
+                breakdowns_it, pad_solver_cycles, solver_its_per_cycle, pad_nonlincycles,fgmres_per_nonlin
+
+
+def find_block_cicelog(lines, startdate, enddate):
+
+    start_block = 0
+    end_block = 0
+
+    for i, line in enumerate(lines):
+
+        if re.search(rf"idate:\s+{startdate}\s+sec:\s+0", line):
+            start_block = i
+        if re.search(rf"idate:\s+{enddate}\s+sec:\s+0", line) and start_block is not None:
+            end_block = i
+            break
+
+    block_lines = lines[start_block:end_block]
+
+    return block_lines
+
+
+
 
 
 
